e2d0258d5a53361358a6327b83b1eed3ba533f5d
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-01-16 18:20:41 +0000

    feat: add `Sidebar.jsx` and `SidebarButton.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import Sidebar from './components/Sidebar'
+
 function App() {
-  return (
-    <h1 className="mt-4 bg-gray-700 p-4 text-center text-white">Hello Word!</h1>
-  )
+  return <Sidebar />
 }
 
 export default App
