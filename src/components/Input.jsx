ef46ab0056c11675c9f3024816f3290e6d5e7b24
Fernando Ferreira <fernandoferreira67@hotmail.com>
2026-01-20 16:18:01 -0300

    perf: ensure inputs are uncontrolled on AddTaskDialog

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,23 @@
+import { forwardRef } from 'react'
+
 import InputLabel from './InputLabel'
 
-const Input = ({ label, errorMessage, ...rest }) => {
+const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
   return (
     <div className="flex flex-col space-y-1 text-left">
       <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
         className="rounded-lg border border-solid border-[#ececec] px-4 py-3 outline-[#00adb5] placeholder:text-sm placeholder:text-[#9a9c9f]"
+        ref={ref}
         {...rest}
       />
       {errorMessage && (
-        <p className="text-left text-xs text-red-500">{errorMessage}</p>
+        <p className="mt-1 text-left text-xs text-red-500">{errorMessage}</p>
       )}
     </div>
   )
-}
+})
+
+Input.displayName = 'Input'
 
 export default Input
